93a135055547eea80826af7c86ef47f783c02b35
tarcisobandeira <tarcisojb@gmail.com>
2020-02-28 21:47:42 -0300

    aaaaaaaaaaaaa

--- src/main/java/br/com/MBean/HomeContasMB.java
+++ src/main/java/br/com/MBean/HomeContasMB.java
@@ -20,6 +20,9 @@ public class HomeContasMB {
 	Calendar calendar = new GregorianCalendar();
 	SimpleDateFormat sdf;
 	List<GerarConta> listGc = new ArrayList<GerarConta>();
+	List<GerarConta> listAberto = new ArrayList<GerarConta>();
+	List<GerarConta> listFechados = new ArrayList<GerarConta>();
+	List<GerarConta> listPendente = new ArrayList<GerarConta>();
 
 	public HomeContasMB() {
 		listGc = gcDAO.listarTodos();
@@ -28,10 +31,11 @@ public class HomeContasMB {
 
 	public void atualizar() {
 		System.out.println("Verificando contas...");
-		for(GerarConta gc : listGc) {
+		for (GerarConta gc : listGc) {
 			calendar = new GregorianCalendar();
 			sdf = new SimpleDateFormat("dd");
-			if ((gc.getConta().getDia_emissao().equals(sdf.format(calendar.getTime())))	&& (!gcDAO.existe(gc.getMes_ano(), gc.getId_conta()))) {
+			if ((gc.getConta().getDia_emissao().equals(sdf.format(calendar.getTime())))
+					&& (!gcDAO.existe(gc.getMes_ano(), gc.getId_conta()))) {
 				sdf = new SimpleDateFormat("MMM/yyyy");
 				calendar.add(Calendar.MONDAY, 1);
 				gc.setMes_ano(sdf.format(calendar.getTime()));
@@ -44,7 +48,7 @@ public class HomeContasMB {
 		}
 		System.out.println("Contas verificadas.");
 	}
-	
+
 	public GerarContaDAO getGcDAO() {
 		return gcDAO;
 	}
@@ -59,6 +63,46 @@ public class HomeContasMB {
 
 	public void setListGc(List<GerarConta> listGc) {
 		this.listGc = listGc;
+	}
+
+	public Calendar getCalendar() {
+		return calendar;
+	}
+
+	public void setCalendar(Calendar calendar) {
+		this.calendar = calendar;
+	}
+
+	public SimpleDateFormat getSdf() {
+		return sdf;
+	}
+
+	public void setSdf(SimpleDateFormat sdf) {
+		this.sdf = sdf;
+	}
+
+	public List<GerarConta> getListAberto() {
+		return listAberto;
+	}
+
+	public void setListAberto(List<GerarConta> listAberto) {
+		this.listAberto = listAberto;
+	}
+
+	public List<GerarConta> getListFechados() {
+		return listFechados;
+	}
+
+	public void setListFechados(List<GerarConta> listFechados) {
+		this.listFechados = listFechados;
+	}
+
+	public List<GerarConta> getListPendente() {
+		return listPendente;
+	}
+
+	public void setListPendente(List<GerarConta> listPendente) {
+		this.listPendente = listPendente;
 	}
 
 }
